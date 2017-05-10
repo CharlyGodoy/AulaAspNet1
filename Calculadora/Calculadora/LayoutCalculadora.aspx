cfeb7bb64a4f41e70675890baf8f77ff04342a34
Aluno <Aluno@045LAB04016>
2017-05-09 22:29:58 -0300

    Calculadora Inicial

---
+++ Calculadora/Calculadora/LayoutCalculadora.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Calculadora.Master" AutoEventWireup="true" CodeBehind="LayoutCalculadora.aspx.cs" Inherits="Calculadora.LayoutCalculadora" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Cabecalho" runat="server">
+    <asp:TextBox ID="txtDisplay" runat="server" Text="Calculadora"></asp:TextBox>    
+
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Principal" runat="server">
+    
+    <div class="row">
+        <div class="col-md-6">
+    <br />
+    <asp:Button ID="btn1" runat="server" Text="1" OnClick="btn1_Click" /> 
+    <asp:Button ID="btn2" runat="server" Text="2" OnClick="btn2_Click"/>
+    <asp:Button ID="btn3" runat="server" Text="3" OnClick="btn3_Click"/>
+    <br />
+    <asp:Button ID="btn4" runat="server" Text="4" OnClick="btn4_Click"/>
+    <asp:Button ID="btn5" runat="server" Text="5" OnClick="btn5_Click"/>
+    <asp:Button ID="btn6" runat="server" Text="6" OnClick="btn6_Click"/>
+    <br />
+    <asp:Button ID="btn7" runat="server" Text="7" OnClick="btn7_Click"/>
+    <asp:Button ID="btn8" runat="server" Text="8" OnClick="btn8_Click"/>
+    <asp:Button ID="btn9" runat="server" Text="9" OnClick="btn9_Click"/>
+    <br />
+    <asp:Button ID="btnVirgula" runat="server" Text="," OnClick="btnVirgula_Click"/>
+    <asp:Button ID="btn0" runat="server" Text="0" OnClick="btn0_Click"/>
+    <asp:Button ID="btnIgual" runat="server" Text="=" OnClick="btnIgual_Click"/>
+    <asp:Button ID="btnMais" runat="server" Text="+" OnClick="btnMais_Click"/>
+    </div>
+        </div>
+
+</asp:Content>
---
+++ Calculadora/Calculadora/LayoutCalculadora.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculadora
+{
+    public partial class LayoutCalculadora : System.Web.UI.Page
+    {
+        public double valor1 { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "1";
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "2";
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "3";
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "4";
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "5";
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "6";
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "7";
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "8";
+        }
+
+        protected void btn9_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "9";
+        }
+
+        protected void btnVirgula_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += ",";
+        }
+
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+            TtxtDysplay.Text += "0";
+        }
+
+        protected void btnIgual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnMais_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
