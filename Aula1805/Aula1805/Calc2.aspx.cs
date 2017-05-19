dc60cc6058ce39b3a3154cd88702fd646f7bda25
Aluno <Aluno@045LAB04016>
2017-05-18 22:07:36 -0300

    Commit Final Funcionando

--- Aula1805/Aula1805/Calc2.aspx.cs
+++ Aula1805/Aula1805/Calc2.aspx.cs
@@ -11,19 +11,29 @@ namespace Aula1805
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            txtValor1Soma.Text = Session["Valor1"].ToString();
-            txtValor2Soma.Text = Session["Valor2"].ToString();
-            txtValor1Sub.Text = Session["Valor1"].ToString();
-            txtValor2Sub.Text = Session["Valor2"].ToString();
-            txtValor1Mult.Text = Session["Valor1"].ToString();
-            txtValor2Mult.Text = Session["Valor2"].ToString();
-            txtValor1Div.Text = Session["Valor1"].ToString();
-            txtValor2Div.Text = Session["Valor2"].ToString();
+            if (!IsPostBack)
+            {
+                if(Session["Valor1"] != null && Session["Valor2"] != null)
+                {
+                    txtValor1Soma.Text = Session["Valor1"].ToString();
+                    txtValor2Soma.Text = Session["Valor2"].ToString();
+                    txtValor1Sub.Text = Session["Valor1"].ToString();
+                    txtValor2Sub.Text = Session["Valor2"].ToString();
+                    txtValor1Mult.Text = Session["Valor1"].ToString();
+                    txtValor2Mult.Text = Session["Valor2"].ToString();
+                    txtValor1Div.Text = Session["Valor1"].ToString();
+                    txtValor2Div.Text = Session["Valor2"].ToString();
 
-            txtResultadoSoma.Text = (Convert.ToDouble(Session["Valor1"]) + Convert.ToDouble(Session["Valor2"])).ToString();
-            txtResultadoSub.Text = (Convert.ToDouble(Session["Valor1"]) - Convert.ToDouble(Session["Valor2"])).ToString();
-            txtResultadoMult.Text = (Convert.ToDouble(Session["Valor1"]) * Convert.ToDouble(Session["Valor2"])).ToString();
-            txtResultadoDiv.Text = (Convert.ToDouble(Session["Valor1"]) / Convert.ToDouble(Session["Valor2"])).ToString();
+                    txtResultadoSoma.Text = (Convert.ToDouble(Session["Valor1"]) + Convert.ToDouble(Session["Valor2"])).ToString();
+                    txtResultadoSub.Text = (Convert.ToDouble(Session["Valor1"]) - Convert.ToDouble(Session["Valor2"])).ToString();
+                    txtResultadoMult.Text = (Convert.ToDouble(Session["Valor1"]) * Convert.ToDouble(Session["Valor2"])).ToString();
+                    txtResultadoDiv.Text = (Convert.ToDouble(Session["Valor1"]) / Convert.ToDouble(Session["Valor2"])).ToString();
+                }else
+                {
+                    Response.Redirect("~/Calc1.aspx");
+                }
+            }
+            
 
         }
     }
