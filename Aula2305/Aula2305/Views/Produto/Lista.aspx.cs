316d86bb059eac04550deea9dc5621344da406dc
Aluno <Aluno@045LAB04016>
2017-05-23 22:14:25 -0300

    Data Base First Model First

---
+++ Aula2305/Aula2305/Views/Produto/Lista.aspx.cs
@@ -0,0 +1,18 @@
+﻿using Aula2305.Models;
+using Aula2305.Views.Base;
+using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace Aula2305.Views.Produto
+{
+    public partial class Lista : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gvProdutos.DataSource = contexto.Produto.ToList();
+
+            gvProdutos.DataBind();
+        }
+    }
+}
