60e1a7c34d5785c419032bd98ff79eec7a1e7820
Aluno <Aluno@045LAB04016>
2017-06-20 21:44:11 -0300

    MVC

---
+++ Wingtip_Toys_MVC/BaseModels/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaseModels
+{
+    public class Produto
+    {
+        [Key]             //Chave primaria
+        public int ProdutoID { get; set; }
+        [Required]        // Campo not null
+        [StringLength(20)]//Tamanho maximo da string
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+
+        // -- Relacionamento Categoria --> Produto
+        public int CategoriaID { get; set; }
+        public virtual Categoria _Categoria { get; set; }
+
+    }
+}
---
+++ Wingtip_Toys_MVC/Web/Controllers/CategoriasController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // Contexto do banco de dados
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Categorias (listagem)
+        public ActionResult Index()
+        {
+            var categorias = db.Categorias.ToList();
+            return View(categorias);
+        }
+    }
+}
